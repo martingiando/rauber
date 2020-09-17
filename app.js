752bda79364f2ebaf62d812e2539aca4504953ae
Martin Giando <martingiando@gmail.com>
2020-09-17 04:17:37 -0300

    firts commit

---
+++ app.js
@@ -0,0 +1,23 @@
+function Latas(name, price){
+    this.name = name;
+    this.price = price;
+}
+
+var simcoe = new Latas('Simcoe Pal Peso Lata', 150);
+var resistencia = new Latas('Resistencia Red Ale Lata', 130);
+var bandida = new Latas('American Ipa Bandid Lataa', 125);
+var golden = new Latas('Golden Ale Lata', 120);
+var angry = new Latas('Angry Honey Rubia Ale Lata', 125);
+var franeri = new Latas('Fra-Ñeri Imperial Stout Lata', 200);
+
+function Pet(name, price){
+    this.name = name;
+    this.price = price;
+}
+
+var simcoePet = new Pet('Simcoe Pal Peso Pet', 170);
+var resistenciaPet = new Pet('Resistencia Red Ale Pet', 160);
+var bandidaPet = new Pet('American Ipa Bandida Pet', 180);
+var goldenPet = new Pet('Golden Ale Pet', 150);
+var angryPet = new Pet('Angry Honey Rubia Ale Pet', 160);
+var fenixPet = new Pet('Fra-Ñeri Imperial Stout Pet', 160);
