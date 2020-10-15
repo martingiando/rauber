c7aa27e525085f339ebcb3f1a8befa94f9c8a600
Martin Giando <martingiando@gmail.com>
2020-10-15 16:06:47 -0300

    actualizacion SIN animacion jQuery y AJAX

--- app.js
+++ app.js
@@ -76,7 +76,6 @@ let baseDeDatos = [
 let $productContainerLatas = document.querySelector('#productContainerLatas')
 let $produtsContainerPet = document.querySelector('#productContainerPet')
 let carrito = localStorage.carrito ? JSON.parse(localStorage.carrito) : [];
-let total = 0
 
 function renderbaseDeDatos() {
     baseDeDatos.forEach(function (producto) {
@@ -94,10 +93,10 @@ function renderbaseDeDatos() {
             </div>
         </div>
         `
-        if ((producto.codigo) < 2){
+        if ((producto.codigo) < 2) {
             $productContainerLatas.appendChild(miNodo)
         }
-        else if ((producto.codigo < 3) && (producto.codigo >= 2)){
+        else if ((producto.codigo < 3) && (producto.codigo >= 2)) {
             $produtsContainerPet.appendChild(miNodo)
         }
     })
@@ -137,16 +136,17 @@ function sectorCompraCervezas() {
     sectorCompraDeProductos.appendChild(miNodoCervezas2)
     sectorCompraDeProductos.appendChild(miNodoCervezas3)
 }
-
 sectorCompraCervezas()
+
 
 const comprarButton = document.querySelector('.comprarButton')
 
 comprarButton.addEventListener('click', comprarButtonClicked);
 
-function comprarButtonClicked(){
+function comprarButtonClicked() {
     compraBirras.innerHTML = '';
-    updateShoppingCartTotal();
+    sumadorDePrecios();
+    restadorDePrecio();
 }
 
 function sectorModal() {
@@ -198,159 +198,92 @@ function sectorModal() {
 sectorModal()
 
 function agregarAlCarritoLatas(index) {
-    
+
     var producto = baseDeDatos[index];
     if (carrito.length > 0) {
         var noExiste = true;
         for (var i = 0; i < carrito.length; i++) {
             if (producto.nombre === carrito[i].nombre) {
-            carrito[i].cantidad++;
-            noExiste = false;
+                carrito[i].cantidad++;
+                noExiste = false;
             }
         }
         if (noExiste) {
             producto.cantidad = 1;
             carrito.push(producto);
         }
-    } 
+    }
     else {
         producto.cantidad = 1;
         carrito.push(producto);
     }
-    
+    sumadorDePrecios();
+    renderCarrito();
     localStorage.carrito = JSON.stringify(carrito);
-}
-
-const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
-
-addToShoppingCartButtons.forEach(addToCartButton => {
-    addToCartButton.addEventListener('click', clickAgregarCerveza);
-});
-
-function clickAgregarCerveza(event) {
-    let button = event.target;
-    let item = button.closest('.item');
-
-    const itemTitle = item.querySelector('.item-title').textContent;
-    const itemPrice = item.querySelector('.item-price').textContent;
-    const itemImage = item.querySelector('.item-image').src;
-
-    addBirras(itemTitle, itemPrice, itemImage)
 }
 
 const compraBirras = document.querySelector('.shoppingCartBirraContainer');
 
-function addBirras() {
-    compraBirras.innerHTML = "";
-    carrito.forEach((cerveza) => {
-      const filaCarrito = document.createElement("div");
-      filaCarrito.innerHTML = `
-      <div class="row shoppingCartItem">
-          <div class="col-6">
-              <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                  <img src=${cerveza.imagen} class="shopping-cart-image">
-                  <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${cerveza.nombre}</h6>
-              </div>
+function renderCarrito() {
+    localStorage.carrito = JSON.stringify(carrito)
+    compraBirras.innerHTML = ``
+    if (carrito.length > 0) {
+        carrito.forEach(cerveza => {
+            compraBirras.innerHTML += `
+            <div class="row shoppingCartItem">
+           <div class="col-6">
+               <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                   <img src=${cerveza.imagen} class="shopping-cart-image">
+                 <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${cerveza.nombre}</h6>
+               </div>
           </div>
-          <div class="col-2">
-              <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                  <p class="item-price mb-0 shoppingCartItemPrice">${cerveza.precio}</p>
-              </div>
-          </div>
-          <div class="col-4">
-              <div
-                  class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                  <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                      value=${cerveza.cantidad}>
-                  <button class="btn btn-danger buttonDelete" type="button">X</button>
-              </div>
-          </div>
-      </div>
-      `;
-      compraBirras.appendChild(filaCarrito);
-      filaCarrito
-        .querySelector(".buttonDelete")
-        .addEventListener("click", removeShoppingCartItem);
-      filaCarrito
-        .querySelector(".shoppingCartItemQuantity")
-        .addEventListener("change", quantityChanged);
-      updateShoppingCartTotal();
-    });
-     // const elementsTitle = compraBirras.getElementsByClassName('shoppingCartItemTitle');
-    // for (let i = 0; i < elementsTitle.length; i++) {
-    //     if (elementsTitle[i].innerText === itemTitle) {
-    //         let elementQuantity = elementsTitle[i].parentElement.parentElement.parentElement.querySelector('.shoppingCartItemQuantity');
-    //         elementQuantity.value++;
-    //         updateShoppingCartTotal();
-    //         return;
-    //     };
-    // }
-    // const filaCarrito = document.createElement('div')
-    // filaCarrito.innerHTML = `
-    // <div class="row shoppingCartItem">
-    //     <div class="col-6">
-    //         <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-    //             <img src=${itemImage} class="shopping-cart-image">
-    //             <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
-    //         </div>
-    //     </div>
-    //     <div class="col-2">
-    //         <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-    //             <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
-    //         </div>
-    //     </div>
-    //     <div class="col-4">
-    //         <div
-    //             class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-    //             <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-    //                 value="1">
-    //             <button class="btn btn-danger buttonDelete" type="button">X</button>
-    //         </div>
-    //     </div>
-    // </div>
-    // `
-    // compraBirras.appendChild(filaCarrito);
-    // filaCarrito.querySelector('.buttonDelete').addEventListener('click', removeShoppingCartItem);
-    // filaCarrito.querySelector('.shoppingCartItemQuantity').addEventListener('change', quantityChanged)
-    // updateShoppingCartTotal();
-}
-
-function updateShoppingCartTotal() {
-    let total = 0;
-    const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
-
-    const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
-
-    shoppingCartItems.forEach(shoppingCartItem => {
-        const shoppingCartItemPriceElement = shoppingCartItem.querySelector('.shoppingCartItemPrice');
-        const shoppingCartItemPrice = Number(shoppingCartItemPriceElement.textContent.replace('$', ''));
-        const shoppingCartItemQuantityElement = shoppingCartItem.querySelector('.shoppingCartItemQuantity');
-        const shoppingCartItemQuantity = Number(shoppingCartItemQuantityElement.value);
-
-        total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
-    });
-    shoppingCartTotal.innerHTML = `$${total.toFixed(0)}`
-}
-
-function removeShoppingCartItem(event) {
-    const buttonClicked = event.target;
-    buttonClicked.closest('.shoppingCartItem').remove();
-    updateShoppingCartTotal();
-}
-
-function quantityChanged(event) {
-    const input = event.target
-    if (input.value <= 0) {
-        input.value = 1
+           <div class="col-2">
+               <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                   <p class="item-price mb-0 shoppingCartItemPrice">${cerveza.precio}</p>
+               </div>
+           </div>
+           <div class="col-4">
+               <div
+                   class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                   <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                       value=${cerveza.cantidad}>
+                   <button class="btn btn-danger buttonDelete" onclick='borradoDeProducto(${carrito.indexOf(cerveza)})' type="button">X</button>
+               </div>
+           </div>
+       </div>
+            `
+        })
     }
-    updateShoppingCartTotal();
+    sumadorDePrecios()
+}
+// renderCarrito();
+
+function borradoDeProducto(index){
+    carrito[index].cantidad = carrito[index].cantidad - 1
+    if (carrito[index].cantidad <= 0){
+        carrito.splice(index, 1)
+    }
+    localStorage.carrito = JSON.stringify(carrito);
+    restadorDePrecio();
+    renderCarrito();
 }
 
-// No salio meter jQuery
-$(document).ready(function () {
-    $(document).scroll(function () { 
-        console.log('Estas scrolleando gil')
-    });
-});
+function sumadorDePrecios(){
+    let total = 0;
+    let precioTotal = document.querySelector('.shoppingCartTotal');
 
-addBirras();
+    carrito.forEach(latita =>{
+        const precio = Number(latita.precio)
+        const cantidad = Number(latita.cantidad)
+        total = total + precio * cantidad
+    })
+    localStorage.carrito = JSON.stringify(carrito);
+    precioTotal.innerHTML = `$${total.toFixed(2)}`
+}
+
+function restadorDePrecio(){
+    carrito.forEach(dejarDeComprar =>{
+        total = sumadorDePrecios() - dejarDeComprar.precio
+    })
+    localStorage.carrito = JSON.stringify(carrito);
+}
