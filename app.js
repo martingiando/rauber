a4b57e1778bd429d1b66a67866dfb0264556a3f7
Martin Giando <martingiando@gmail.com>
2020-11-02 20:29:50 -0300

    proyecto final

--- app.js
+++ app.js
@@ -1,88 +1,49 @@
-let baseDeDatos = [
-    {
-        nombre: 'Simcoe Pal Peso Lata',
-        precio: 150,
-        imagen: 'img/simcoePalPesoMain.jpg',
-        codigo: 1.1,
-    },
-    {
-        nombre: 'Resistencia Red Ale Lata',
-        precio: 130,
-        imagen: 'img/resistenciaMain.jpg',
-        codigo: 1.2,
-    },
-    {
-        nombre: 'American Ipa Bandida Lata',
-        precio: 125,
-        imagen: 'img/IpaBandidamain.jpg',
-        codigo: 1.3,
-    },
-    {
-        nombre: 'Golden Ale Lata',
-        precio: 120,
-        imagen: 'img/IpaBandidamain.jpg',
-        codigo: 1.4,
-    },
-    {
-        nombre: 'Angry Honey Rubia Ale Lata',
-        precio: 125,
-        imagen: 'img/IpaBandidamain.jpg',
-        codigo: 1.5,
-    },
-    {
-        nombre: 'Fra-Ñeri Imperial Stout Lata',
-        precio: 200,
-        imagen: 'img/IpaBandidamain.jpg',
-        codigo: 1.6,
-    },
-    {
-        nombre: 'Simcoe Pal Peso Pet',
-        precio: 170,
-        imagen: 'img/simcoePalPesoMain.jpg',
-        codigo: 2.1,
-    },
-    {
-        nombre: 'Resistencia Red Ale Pet',
-        precio: 160,
-        imagen: 'img/resistenciaMain.jpg',
-        codigo: 2.2,
-    },
-    {
-        nombre: 'American Ipa Bandida Pet',
-        precio: 180,
-        imagen: 'img/IpaBandidamain.jpg',
-        codigo: 2.3,
-    },
-    {
-        nombre: 'Golden Ale Pet',
-        precio: 150,
-        imagen: 'img/IpaBandidamain.jpg',
-        codigo: 2.4,
-    },
-    {
-        nombre: 'Angry Honey Rubia Ale Pet',
-        precio: 160,
-        imagen: 'img/IpaBandidamain.jpg',
-        codigo: 2.5,
-    },
-    {
-        nombre: 'Fenix Hop Pet',
-        precio: 160,
-        imagen: 'img/IpaBandidamain.jpg',
-        codigo: 2.6,
+/* ------------------- ARRAY DE BASE DE DATOS QUE VA A SER ------------------ */
+/* ------------------------ COMPLETADO LUEGO CON AJAX ----------------------- */
+
+let baseDeDatos = [];
+
+document.addEventListener('DOMContentLoaded', cargaInicial);
+
+/* -------------------------------------------------------------------------- */
+/*             FUNCION CON AJAX PARA RENDERIZAR LA DB Y EL CARRITO            */
+/* -------------------------------------------------------------------------- */
+
+function cargaInicial() {
+    $.ajax({
+        url: 'db.json',
+        success: function (data) {
+            console.log(data)
+            baseDeDatos = data;
+            renderbaseDeDatos();
+            renderCarrito();
+        },
+        error: function (error, jqXHR, status) {
+            console.log(error);
+        }
     }
-]
+    )
+}
+
+
+/* ---------------- CREO LAS VARIABLES QUE VAN A CONTENER LOS --------------- */
+/* --------------------- DIV CREADOS EN HTML CON SUS ID --------------------- */
 
 let $productContainerLatas = document.querySelector('#productContainerLatas')
 let $produtsContainerPet = document.querySelector('#productContainerPet')
+
 let carrito = localStorage.carrito ? JSON.parse(localStorage.carrito) : [];
+
+/* -------------------------------------------------------------------------- */
+/*                      FUNCION QUE ME DIAGRAMA LAS CARDS                     */
+/* -------------------------------------------------------------------------- */
 
 function renderbaseDeDatos() {
     baseDeDatos.forEach(function (producto) {
         var miNodo = document.createElement('div')
         miNodo.classList.add('col', 'mb-4')
         miNodo.innerHTML = `
-        <div class="card item">
+        <div class="card item" style="opacity:0">
             <h5 class="card-title text-center item-title">${producto.nombre}</h5>
             <p class="card-t1ext text-center precioCard item-price">$${producto.precio}</p>
             <img src="${producto.imagen}" class="card-img-top item-image" alt="...">
@@ -105,7 +66,13 @@ function renderbaseDeDatos() {
 renderbaseDeDatos();
 
 
+/* ------------- VARIABLE SELECCIONADA PARA LA DIAGRAMAR CARRITO ------------ */
+
 let sectorCompraDeProductos = document.querySelector('#compras')
+
+/* -------------------------------------------------------------------------- */
+/*                   FUNCION QUE ME DIAGRAMA SECTOR CARRITO                   */
+/* -------------------------------------------------------------------------- */
 
 function sectorCompraCervezas() {
     let miNodoCervezas1 = document.createElement('div')
@@ -138,64 +105,9 @@ function sectorCompraCervezas() {
 }
 sectorCompraCervezas()
 
-
-const comprarButton = document.querySelector('.comprarButton')
-
-comprarButton.addEventListener('click', comprarButtonClicked);
-
-function comprarButtonClicked() {
-    compraBirras.innerHTML = '';
-    sumadorDePrecios();
-    restadorDePrecio();
-}
-
-function sectorModal() {
-    let miNodoModal = document.createElement('div')
-    miNodoModal.classList.add('row')
-    miNodoModal.innerHTML = `
-    <div class="modal fade" id="comprarModal" tabindex="-1" aria-labelledby="comprarModalLabel"
-          aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content">
-                  <div class="modal-header">
-                      <h5 class="modal-title" id="comprarModalLabel">Gracias por su compra</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                      </button>
-                  </div>
-                  <div class="modal-body">
-                  <form>
-                  <div class="form-row">
-                    <div class="form-group col-md-6">
-                      <label for="inputEmail4">Email</label>
-                      <input type="email" class="form-control" id="inputEmail4">
-                    </div>
-                    <div class="form-group col-md-6">
-                      <label for="inputPassword4">Nombre y Apellido</label>
-                      <input type="text" class="form-control" id="inputPassword4">
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                  </div>
-                  <div class="form-group">
-                    <label for="inputAddress2">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                  </div>
-                </form>
-                  </div>
-                  <div class="modal-footer">
-                      <button type="button" class="btn btn-success" data-dismiss="modal">Confirmar Compra</button>
-                  </div>
-              </div>
-          </div>
-    </div>
-    `
-    sectorCompraDeProductos.appendChild(miNodoModal)
-
-}
-sectorModal()
+/* -------------------------------------------------------------------------- */
+/*                   FUNCION QUE ME PUSHEA AL ARRAY CARRITO                   */
+/* -------------------------------------------------------------------------- */
 
 function agregarAlCarritoLatas(index) {
 
@@ -220,13 +132,25 @@ function agregarAlCarritoLatas(index) {
     sumadorDePrecios();
     renderCarrito();
     localStorage.carrito = JSON.stringify(carrito);
+    Swal.fire({
+        position: 'top-end',
+        toast: true,
+        icon: 'success',
+        title: 'El producto se agrego al carrito',
+        showConfirmButton: false,
+        timer: 1500
+      })
 }
 
 const compraBirras = document.querySelector('.shoppingCartBirraContainer');
 
+/* -------------------------------------------------------------------------- */
+/*        FUNCION QUE ME DIAGRAMA EL ARRAY CARRITO EN EL SECTOR CARRITO       */
+/* -------------------------------------------------------------------------- */
+
 function renderCarrito() {
     localStorage.carrito = JSON.stringify(carrito)
-    compraBirras.innerHTML = ``
+    compraBirras.innerHTML = ''
     if (carrito.length > 0) {
         carrito.forEach(cerveza => {
             compraBirras.innerHTML += `
@@ -245,8 +169,7 @@ function renderCarrito() {
            <div class="col-4">
                <div
                    class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                   <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                       value=${cerveza.cantidad}>
+                   x${cerveza.cantidad}
                    <button class="btn btn-danger buttonDelete" onclick='borradoDeProducto(${carrito.indexOf(cerveza)})' type="button">X</button>
                </div>
            </div>
@@ -258,9 +181,11 @@ function renderCarrito() {
 }
 // renderCarrito();
 
-function borradoDeProducto(index){
+/* ------------------ FUNCION PARA BORRAR PROD DEL CARRITO ------------------ */
+
+function borradoDeProducto(index) {
     carrito[index].cantidad = carrito[index].cantidad - 1
-    if (carrito[index].cantidad <= 0){
+    if (carrito[index].cantidad <= 0) {
         carrito.splice(index, 1)
     }
     localStorage.carrito = JSON.stringify(carrito);
@@ -268,22 +193,100 @@ function borradoDeProducto(index){
     renderCarrito();
 }
 
-function sumadorDePrecios(){
+/* ------------------------ FUNCION QUE SUMA EL TOTAL ----------------------- */
+
+function sumadorDePrecios() {
     let total = 0;
     let precioTotal = document.querySelector('.shoppingCartTotal');
 
-    carrito.forEach(latita =>{
+    carrito.forEach(latita => {
         const precio = Number(latita.precio)
         const cantidad = Number(latita.cantidad)
-        total = total + precio * cantidad
+        return total = total + precio * cantidad
     })
     localStorage.carrito = JSON.stringify(carrito);
     precioTotal.innerHTML = `$${total.toFixed(2)}`
 }
 
-function restadorDePrecio(){
-    carrito.forEach(dejarDeComprar =>{
+/* ----------------------- FUNCION QUE RESTA EL TOTAL ----------------------- */
+
+function restadorDePrecio() {
+    carrito.forEach(dejarDeComprar => {
         total = sumadorDePrecios() - dejarDeComprar.precio
     })
     localStorage.carrito = JSON.stringify(carrito);
 }
+
+/* ----------- FUNCION QUE ME VACIA EL CARRITO AL CONFIRMAR COMPRA ---------- */
+
+function terminarCompra(){
+    carrito = []
+    sumadorDePrecios()
+    compraBirras.innerHTML = '';
+}
+
+/* ---------------------- FUNCION QUE DIAGRAMA EL MODAL --------------------- */
+
+function sectorModal() {
+    let miNodoModal = document.createElement('div')
+    miNodoModal.classList.add('row')
+    miNodoModal.innerHTML = `
+    <div class="modal fade" id="comprarModal" tabindex="-1" aria-labelledby="comprarModalLabel"
+    aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="comprarModalLabel">Gracias por su compra</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="#" method="POST" id="my-form">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputEmail4">Email</label>
+                                <input type="email" class="form-control" id="inputEmail4" name="_replayto">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputPassword4">Nombre y Apellido</label>
+                                <input type="text" class="form-control" id="inputPassword4" name="name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputAddress">Address</label>
+                                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputAddress2">Address 2</label>
+                                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-success" onclick="terminarCompra()" data-dismiss="modal" value="Send" id="my-form-button">Confirmar Compra</button>
+                    <p id="my-form-status"></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+    
+    sectorCompraDeProductos.appendChild(miNodoModal)
+    
+}
+sectorModal()
+
+/* ------------------------- ANIMACION DE LAS CARDS ------------------------- */
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+      $(".card.item").each(function (i) {
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if (bottom_of_window > bottom_of_object) {
+          $(this).animate({ opacity: "1" }, 1500);
+        }
+      });
+    });
+  });
